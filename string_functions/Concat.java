9d0e8c58876bdbc63647ce7eb1a1da092d767504
RAJESH PANIGRAHI <rajeshpanigrahi098@gmail.com>
2023-02-06 22:39:26 +0530

    5

--- string_functions/Concat.java
+++ string_functions/Concat.java
@@ -1,7 +1,8 @@
 public class Concat {
-public static void main(String[] args){
-        String name="what do you know about me";
+    public static void main(String[] args) {
+        String name = "what do you know about me";
         System.out.println(name.contains("do you know"));
         System.out.println(name.contains("about"));
         System.out.println(name.contains("hello"));
-}}
+    }
+}
