b4fd7ce935f5029c826402ba1050f029b618043e
RAJESH PANIGRAHI <rajeshpanigrahi098@gmail.com>
2023-02-06 22:31:20 +0530

    3 commit

--- string_functions/functionss/CharAt.java
+++ string_functions/functionss/CharAt.java
@@ -1,11 +1,13 @@
 package functionss;
 
-public class CharAt {
-    public class CharAtExample {
-        public static void main(String args[]) {
-            String name = "javatpoint";
-            char ch = name.charAt(4);//returns the char value at the 4th index
-            System.out.println(ch);
-        }
-    }
+public class C {
+    public class CompareToExample2{
+        public static void main(String args[]){
+            String s1="hello";
+            String s2="";
+            String s3="me";
+            System.out.println(s1.compareTo(s2));
+            System.out.println(s2.compareTo(s3));
+        }}
 }
+
